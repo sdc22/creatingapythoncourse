9d11c150f471c52d7f601c018c06d229b0cf6632
sdc22 <32734353+sdc22@users.noreply.github.com>
2018-02-12 17:45:09 +0200

    Initial course creation commit DataCamp

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+# If bash command fails, build should error out
+set -e
+
+##### Install specific package versions with pip #####
+
+# pip3 install pandas==0.19.1
+# pip3 install tweepy==3.5.0
+# pip3 install bs4==4.4.1
+
+# NOTE: matplotlib 1.5.3 onward is NOT currently supported. The image comes installed with matplotlib 1.5.1
